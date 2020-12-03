f421ba6dcdb3702d41bef96127b16d0430afad36
iamchampi0n260799 <59090221+iamchampi0n260799@users.noreply.github.com>
2020-12-03 22:44:36 +0700

    fix categories api and coursepage

--- src/containers/Home/CategoriesPage/index.js
+++ src/containers/Home/CategoriesPage/index.js
@@ -2,31 +2,84 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { actFetchCategories } from "./modules/action";
 import CourseItems from "../../../components/CourseItems";
+import Search from '../../../components/Search'
+import "./index.css"
+import axios from "axios";
+import { Link } from "react-router-dom";
 class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: null,
+      categories: [],
     };
   }
   componentDidMount() {
-    let { categories } = this.props.match.params; // backend
+    let { categories } = this.props.match.params; 
     this.props.fetchCategories(categories)
+    const url = `https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhMucKhoaHoc`;
+    axios
+      .get(url)
+      .then((result) => {
+        this.setState({
+          categories: result.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+  componentDidUpdate(prevProps) {
+    if(this.props.match !== prevProps.match){
+      this.props.fetchCategories(this.props.match.params.categories)
+    }
+  }
+  
   renderCourse = () => {
-    const { data } = this.props;
+    let { data, searchTerm } = this.props;
     if (data) {
+      data = data.filter((data) => {
+        return (
+          data.tenKhoaHoc.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+        );
+      });
       return data.map((course, index) => (
         <CourseItems key={index} course={course} />
       ));
     }
   };
+  handleClickToPush = (maDanhMuc) => {
+    this.props.history.push(`/categories/${maDanhMuc}`);
+  };
+  renderCategoies = () => {
+    return this.state.categories.map((item, index) => (
+      <Link
+        style={{ cursor: "pointer" }}
+        key={index}
+        onClick={() => {
+          this.handleClickToPush(item.maDanhMuc);
+        }}
+        className="dropdown-item"
+        // to={`/courses/${item.maDanhMuc}`}
+      >
+        {item.tenDanhMuc}
+      </Link>
+    ));
+  };
   render() {
     console.log(this.state.categories);
     return (
-      <div style={{ display: "flex" }} className="container-fluid mt-5">
-        {this.renderCourse()}
+      <div className="">
+      <div className="row">
+        <div className="categories col-2">
+          {this.renderCategoies()}
+        </div>
+        <div className="coursepage col-10">
+
+          <Search />
+          <div className="row">{this.renderCourse()}</div>
+        </div>
       </div>
+    </div>
     );
   }
 }
@@ -35,6 +88,7 @@ const mapStateToProps = (state) => {
     loading: state.categoriesReducer.loading,
     data: state.categoriesReducer.data,
     error: state.categoriesReducer.error,
+    searchTerm: state.courseReducer.searchTerm,
   };
 };
 const mapDispatchToProps = (dispatch) => {
--- src/containers/Home/CoursesPage/index.js
+++ src/containers/Home/CoursesPage/index.js
@@ -1,53 +1,96 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import CourseItems from "../../../components/CourseItems";
-import Search from '../../../components/Search'
+import Search from "../../../components/Search";
 import { actionFetchCourse } from "./modules/action";
-import "./index.css"
+import "./index.css";
 class Courses extends Component {
-    componentDidMount() {
-        this.props.getCourse();
-      }
-    
-      renderCourse = () => {
-        let { data, searchTerm } = this.props;
-        let timkiem = searchTerm
-        if (data) {
-          data = data.filter((data) => {
-            return (
-              data.tenKhoaHoc.toLowerCase().indexOf(timkiem.toLowerCase()) !== -1
-            );
-          });
-          return data.map((course, index) => (
-            <CourseItems key={index} course={course} />
-          ));
-        }
-      };
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+  componentDidMount() {
+    this.props.getCourse();
+    const url = `https://elearning0706.cybersoft.edu.vn/api/QuanLyKhoaHoc/LayDanhMucKhoaHoc`;
+    axios
+      .get(url)
+      .then((result) => {
+        this.setState({
+          categories: result.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  renderCourse = () => {
+    let { data, searchTerm } = this.props;
+    let timkiem = searchTerm;
+    if (data) {
+      data = data.filter((data) => {
         return (
-            <div className="coursepage">
-              <h2>Tất cả khóa học:</h2>
-              <Search />
-               <div className="row">
-                   {this.renderCourse()}
-               </div>
-            </div>
-        )
+          data.tenKhoaHoc.toLowerCase().indexOf(timkiem.toLowerCase()) !== -1
+        );
+      });
+      return data.map((course, index) => (
+        <CourseItems key={index} course={course} />
+      ));
     }
+  };
+
+  handleClickToPush = (maDanhMuc) => {
+    this.props.history.push(`/categories/${maDanhMuc}`);
+  };
+  renderCategoies = () => {
+    return this.state.categories.map((item, index) => (
+      <Link
+        style={{ cursor: "pointer" }}
+        key={index}
+        onClick={() => {
+          this.handleClickToPush(item.maDanhMuc);
+        }}
+        className="dropdown-item"
+        // to={`/courses/${item.maDanhMuc}`}
+      >
+        {item.tenDanhMuc}
+      </Link>
+    ));
+  };
+  render() {
+    return (
+      <div className="">
+        <div className="row">
+          <div className="categories col-2">
+            {this.renderCategoies()}
+          </div>
+          <div className="coursepage col-10">
+
+            <Search />
+            <div className="row">{this.renderCourse()}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => {
-    return {
-      loading: state.courseReducer.loading,
-      data: state.courseReducer.data,
-      error: state.courseReducer.error,
-      searchTerm: state.courseReducer.searchTerm,
-    };
+  return {
+    loading: state.courseReducer.loading,
+    data: state.courseReducer.data,
+    error: state.courseReducer.error,
+    searchTerm: state.courseReducer.searchTerm,
   };
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      getCourse: () => {
-        dispatch(actionFetchCourse());
-      },
-    };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getCourse: () => {
+      dispatch(actionFetchCourse());
+    },
   };
-  export default connect(mapStateToProps, mapDispatchToProps)(Courses);
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Courses);
