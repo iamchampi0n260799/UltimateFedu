50333ffc209fb48a5bdc546b85f7a06846706e0f
iamchampi0n260799 <59090221+iamchampi0n260799@users.noreply.github.com>
2021-01-20 19:52:40 +0700

    Updates

---
+++ precache-manifest.7d9eba03d14be08bc73e3171cb0eb6a7.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fe8c9fe009c2d4b0d123a09737cd12",
+    "url": "/UltimateFedu/index.html"
+  },
+  {
+    "revision": "20978df8d0af8afadbb2",
+    "url": "/UltimateFedu/static/css/10.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "be7cdb34a67be9575189",
+    "url": "/UltimateFedu/static/css/11.e760920a.chunk.css"
+  },
+  {
+    "revision": "fc734bef6af6ca2ac3e0",
+    "url": "/UltimateFedu/static/css/14.ed7516c9.chunk.css"
+  },
+  {
+    "revision": "f9d4e916801cbcb5c5f6",
+    "url": "/UltimateFedu/static/css/15.07a3017a.chunk.css"
+  },
+  {
+    "revision": "6644d1fabe034b80e925",
+    "url": "/UltimateFedu/static/css/16.98547f7f.chunk.css"
+  },
+  {
+    "revision": "5d3362bdc593c8d5617a",
+    "url": "/UltimateFedu/static/css/17.0bbe403e.chunk.css"
+  },
+  {
+    "revision": "b48e60bf9a59987d9e60",
+    "url": "/UltimateFedu/static/css/18.6ac6ca4b.chunk.css"
+  },
+  {
+    "revision": "acd6baff85b0ea8d2c69",
+    "url": "/UltimateFedu/static/css/20.15b1f6d8.chunk.css"
+  },
+  {
+    "revision": "098d93e9fc7f7bada641",
+    "url": "/UltimateFedu/static/css/21.3875c413.chunk.css"
+  },
+  {
+    "revision": "a6c3efeab584eaacb843",
+    "url": "/UltimateFedu/static/css/22.efb15903.chunk.css"
+  },
+  {
+    "revision": "cff7c0de4dc5db129e37",
+    "url": "/UltimateFedu/static/css/5.16af3ec6.chunk.css"
+  },
+  {
+    "revision": "392e47cbc6015baf7ab4",
+    "url": "/UltimateFedu/static/css/6.fd68bc05.chunk.css"
+  },
+  {
+    "revision": "5c073ce8e226b84a4a49",
+    "url": "/UltimateFedu/static/css/main.792d1a3b.chunk.css"
+  },
+  {
+    "revision": "ecbf20797d27f97cb207",
+    "url": "/UltimateFedu/static/js/0.084f9609.chunk.js"
+  },
+  {
+    "revision": "29b7f33be1e7eee9872e",
+    "url": "/UltimateFedu/static/js/1.21b96b6c.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/UltimateFedu/static/js/1.21b96b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20978df8d0af8afadbb2",
+    "url": "/UltimateFedu/static/js/10.4822afe9.chunk.js"
+  },
+  {
+    "revision": "6fc17f07ab04e757a540489a12ab8cb3",
+    "url": "/UltimateFedu/static/js/10.4822afe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7cdb34a67be9575189",
+    "url": "/UltimateFedu/static/js/11.91b2d829.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/UltimateFedu/static/js/11.91b2d829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a8e3ca3aa29c530ce2",
+    "url": "/UltimateFedu/static/js/12.0a8675a4.chunk.js"
+  },
+  {
+    "revision": "698c38c07e479f892110",
+    "url": "/UltimateFedu/static/js/13.1a01d17a.chunk.js"
+  },
+  {
+    "revision": "fc734bef6af6ca2ac3e0",
+    "url": "/UltimateFedu/static/js/14.ddbb16df.chunk.js"
+  },
+  {
+    "revision": "f9d4e916801cbcb5c5f6",
+    "url": "/UltimateFedu/static/js/15.79895565.chunk.js"
+  },
+  {
+    "revision": "6644d1fabe034b80e925",
+    "url": "/UltimateFedu/static/js/16.bd9222dd.chunk.js"
+  },
+  {
+    "revision": "5d3362bdc593c8d5617a",
+    "url": "/UltimateFedu/static/js/17.06025f85.chunk.js"
+  },
+  {
+    "revision": "b48e60bf9a59987d9e60",
+    "url": "/UltimateFedu/static/js/18.a9052803.chunk.js"
+  },
+  {
+    "revision": "62bb09b2e8705f29fc94",
+    "url": "/UltimateFedu/static/js/19.41bd5174.chunk.js"
+  },
+  {
+    "revision": "c1ae3a153ee15098abc0",
+    "url": "/UltimateFedu/static/js/2.c8b046a8.chunk.js"
+  },
+  {
+    "revision": "acd6baff85b0ea8d2c69",
+    "url": "/UltimateFedu/static/js/20.c59d2019.chunk.js"
+  },
+  {
+    "revision": "098d93e9fc7f7bada641",
+    "url": "/UltimateFedu/static/js/21.fae285d5.chunk.js"
+  },
+  {
+    "revision": "a6c3efeab584eaacb843",
+    "url": "/UltimateFedu/static/js/22.f1680871.chunk.js"
+  },
+  {
+    "revision": "a4b5f70099d1cc93acb5",
+    "url": "/UltimateFedu/static/js/23.d2becf0b.chunk.js"
+  },
+  {
+    "revision": "bf579845141f918c314b",
+    "url": "/UltimateFedu/static/js/3.10e8d685.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/UltimateFedu/static/js/3.10e8d685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7935a1744e0bff902e",
+    "url": "/UltimateFedu/static/js/4.a225796e.chunk.js"
+  },
+  {
+    "revision": "cff7c0de4dc5db129e37",
+    "url": "/UltimateFedu/static/js/5.76591b34.chunk.js"
+  },
+  {
+    "revision": "392e47cbc6015baf7ab4",
+    "url": "/UltimateFedu/static/js/6.6e6d89c9.chunk.js"
+  },
+  {
+    "revision": "a71bce3f0ef01f647f50",
+    "url": "/UltimateFedu/static/js/9.7f9ebb25.chunk.js"
+  },
+  {
+    "revision": "5c073ce8e226b84a4a49",
+    "url": "/UltimateFedu/static/js/main.3f3ff68b.chunk.js"
+  },
+  {
+    "revision": "09c2eaf7f72f87c27fdb",
+    "url": "/UltimateFedu/static/js/runtime-main.883271f0.js"
+  },
+  {
+    "revision": "78a58b04f54e2089342bfbfb50aea4d1",
+    "url": "/UltimateFedu/static/media/carousel-01.78a58b04.jpg"
+  },
+  {
+    "revision": "ad85eef7d3c48b8f5441135cc10197a3",
+    "url": "/UltimateFedu/static/media/funfact_bg.ad85eef7.png"
+  },
+  {
+    "revision": "51b25458e2af3b0cb0f41590f20766b6",
+    "url": "/UltimateFedu/static/media/learning.51b25458.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/UltimateFedu/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/UltimateFedu/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/UltimateFedu/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/UltimateFedu/static/media/slick.f97e3bbf.svg"
+  }
+]);
